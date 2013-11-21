388999ecef12a5c95b367fc138a19d8280334ea5
NickyH <nicky.hughes@tapestry.net>
2013-11-21 11:22:30 +1100

    actions panel changes

--- js/override.js
+++ js/override.js
@@ -24,6 +24,17 @@ $(function(){
 //   $('div .oval').toggleClass('rotate');
 // }
 
+function toggle_action_required() {
+  if ($(this).prop('checked')) {
+    $(this).parents('.form-group').next('.form-group').toggleClass('hidden');
+    $('button[data-id="referral-to"]').removeClass('disabled');
+  }
+  if (!($(this).prop('checked'))) {
+    $(this).parents('.form-group').next('.form-group').toggleClass('hidden');
+    $('button[data-id="referral-to"]').addClass('disabled');
+  }
+}
+
 function table_search(thisObj, tableID) {
 var $rows = $("#"+tableID+" tr");
 
